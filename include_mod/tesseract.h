413ea777a309e46e8d7b1e579ea9fa36cf58f87a
willus <noreply@willus.com>
2013-09-02 06:09:00 -0400

    Version 2.00

--- include_mod/tesseract.h
+++ include_mod/tesseract.h
@@ -1,24 +1,9 @@
 /*
-** tesseract.h     Include file for willus.com Tesseract C API.
-**                 Last udpated 9-1-12
+** Willus.com's Tesseract C Wrappers
 **
-** Copyright (C) 2012  http://willus.com
-**
-** This program is free software: you can redistribute it and/or modify
-** it under the terms of the GNU Affero General Public License as
-** published by the Free Software Foundation, either version 3 of the
-** License, or (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU Affero General Public License for more details.
-**
-** You should have received a copy of the GNU Affero General Public License
-** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+** 6-8-12
 **
 */
-
 
 #ifndef           _TESSERACT_H_
 #define           _TESSERACT_H_
